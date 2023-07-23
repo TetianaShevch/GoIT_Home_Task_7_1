aec08a3c08afceef1f5f1dd7e3cc1e6db84f4521
TetianaShevch <139060254+TetianaShevch@users.noreply.github.com>
2023-07-23 12:26:10 +0300

    First Commit

---
+++ clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='Clean_folder',
+    version='0.0.1',
+    description='Sorting of folder',
+    url='https://github.com/TetianaShevch/GoIT_Home_task_7.git',
+    author='Tetiana Shevchenko',
+    author_email='Shevch_Tatyana@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder=clean_folderclean:clean']}
+    )
